38c1fb4fbcea671dcaf3ccf06dd8701b3fbc65df
212574163 <omer.elhiraika@ge.com>
2018-10-22 11:58:13 -0700

    Added new App Keys for Android and iOS

--- PredixSDKXamarinDemos/PushNotificationsDemo/DataAccess/Constants.cs
+++ PredixSDKXamarinDemos/PushNotificationsDemo/DataAccess/Constants.cs
@@ -4,8 +4,8 @@ namespace PushNotificationsDemo
     public static class Constants
     {
         public static Uri PushServiceURL = new Uri("https://push-notification.run.aws-usw02-dev.ice.predix.io/notification/register");
-        public static string AndroidAppKey = "5e25e162-f0b6-4eea-9ce2-ccc7fd40e246";
-        public static string IOSAppKey = "21ddbcd0-9279-48bd-8784-50234b6a3820";
+        public static string AndroidAppKey = "9881c302-de60-452e-820b-058ebcc8d4d3";
+        public static string IOSAppKey = "c5252da3-0364-4fd3-ae98-c9365fde4f27";
 
         public static Uri UaaServer = new Uri("https://39c2c0ae-6e65-4f38-adfc-349d158ecc4d.predix-uaa.run.aws-usw02-pr.ice.predix.io");
         public static Uri RedirectUri = new Uri("predixsdk://predixsdkforios.io/authorization_grant");
